3eb69cd8919d18b1cb70467a8d4f33e5b3e42186
ilyatfi <ilya05@inbox.lv>
2022-08-23 16:32:25 +0300

    contact us form styling

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -1,16 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="content-wrapper contact">
-    <h2>Contact us</h2>
-    <form>
-        <div class="user-details">
-            <input type="text" id="name" name="name" placeholder="Name">
-            <input type="text" id="email" name="email" placeholder="Email">
+<div class="content-wrapper">
+    <div class="contact-card">
+        <div class="card-header">{{ __('Contact us') }}</div>
+
+        <div class="card-body">
+            <form>
+                <div class="user-details">
+                    <input type="text" id="name" name="name" placeholder="Name">
+                    <input type="text" id="email" name="email" placeholder="Email">
+                </div>
+                <input type="text" id="telephone" name="telephone" placeholder="Phone number">
+                <textarea type="text" id="comment" name="comment" placeholder="Comment"> </textarea>
+                <div class="btn-wrap">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Submit') }}
+                    </button>
+                </div>
+            </form>
         </div>
-        <input type="text" id="telephone" name="telephone" placeholder="Phone number">
-        <textarea type="text" id="comment" name="comment" placeholder="Comment"> </textarea>
-    </form>
+    </div>
 </div>
 
 @endsection
