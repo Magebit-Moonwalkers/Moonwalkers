c188fdda1828d86a340942dd24e54b9dce6b2906
liskasi <lisasirotina@gmail.com>
2022-08-23 11:43:00 +0300

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
