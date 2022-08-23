50611010fde1e270c524b9ebbae392d2ac526e0b
liskasi <lisasirotina@gmail.com>
2022-08-23 10:16:25 +0300

    added cart controller

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,34 @@ Route::get('/about', function () {
 Route::get('/contact-us', function () {
     return view('contact-us');
 });
+
+Route::get('/products', function () {
+    $products = DB::table('products')->get();
+    return view('admin-products', compact('products'));
+})->name('modify-products');
+
+Route::put('/products', function () {
+    $products = DB::table('products')->get();
+    return view('admin-products', compact('products'));
+})->name('modify-products');
+
+Route::post('/products', function () {
+    $products = DB::table('products')->get();
+    return view('admin-products', compact('products'));
+})->name('modify-products');
+
+Route::get('/add-product', function () {
+    return view('admin-addProductForm');
+})->name('add-product');
+
+Route::get('product/{id}',[ProductController::class, 'showById'])->name('product.show');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('cart', [CartController::class, 'cartList'])->name('cart.list');
+Route::post('cart', [CartController::class, 'addToCart'])->name('cart.store');
+Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.update');
+Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
+Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
