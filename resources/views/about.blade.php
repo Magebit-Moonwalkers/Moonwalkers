a41d2e77b217745275901aa53217a1e66e4b41cb
liskasi <lisasirotina@gmail.com>
2022-08-23 11:35:37 +0300

    Added background, about and contact us pages (still in process)

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="content-wrapper about">
+    <div class="images">
+        <img src="https://www.ieskaties.lv/image/cache/catalog/ext4oc/2592038772-3179022424-3392-468x468.jpg" alt="">
+        <img src="https://www.ieskaties.lv/image/cache/catalog/ext4oc/2592038772-3179022424-3392-468x468.jpg" alt="">
+        <img src="https://www.ieskaties.lv/image/cache/catalog/ext4oc/2592038772-3179022424-3392-468x468.jpg" alt="">
+    </div>
+    <div class="about-us">
+        <h2>About</h2>
+        <p>
+            We are the biggest retailer of optics telescopes, binoculars, riflescopes, spotting scopes, microscopes and all other optical parts in Baltic Market. This webshop are for people interested in astronomy, science, hunting, nature exploration, animal research, security and other activities. We are offering the main brands of American, Asian and European producers providing high quality. We offer full warranty and support for all products bought in our store.
+        </p>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/contact-us.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="content-wrapper contact">
+    <h2>Contact us</h2>
+    <div class="working-hours">Working hours</div>
+    <form>
+        <div class="user-details">
+            <input type="text" id="name" name="name" placeholder="Name">
+            <input type="text" id="email" name="email" placeholder="Email">
+        </div>
+        <input type="text" id="telephone" name="telephone" placeholder="Phone number">
+        <textarea type="text" id="comment" name="comment" placeholder="Comment"> </textarea>
+    </form>
+</div>
+
+@endsection
