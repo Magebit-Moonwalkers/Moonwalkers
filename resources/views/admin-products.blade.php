eb8f82077bbfe111440aaf4ac139f0681e6283aa
ilyatfi <ilya05@inbox.lv>
2022-08-25 14:06:54 +0300

    admin products styling

--- resources/views/admin-products.blade.php
+++ resources/views/admin-products.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.app')
+
 @section('content')
-    <div class="container">
-    <br><br><br><br>
 
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+@vite(['resources/sass/admin_products.scss'])
 
-                <a href="{{route('add-product')}}" style="color:red;">Add product</a>
+    <div class="content">
+
+        <div class="content-wrapper admin-products">
+            <div class="add-products-wrap">
+
+                <a href="{{route('add-product')}}" class="add-product-btn">Add product</a>
+
                 <div class="products">
 
                 @foreach($products as $product)
-                    <div><a href="{{ route('admin.product.show', $product->product_id) }}">
-                        <?php echo $product->name; ?>
-                    </a></div>
+                    <div class="product">
+                        <a href="{{ route('admin.product.show', $product->product_id) }}">
+                            <?php echo $product->name; ?>
+                        </a>
+                    </div>
                 @endforeach
                 </div>
             </div>
