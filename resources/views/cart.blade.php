02ef356491d63ab896e804656d29c8df28074427
“SantaLauva” <santalauva089@gmail.com>
2022-08-26 07:03:13 +0300

    display of cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,96 +1,55 @@
 @extends('layouts.app')
 
+@vite(['resources/sass/cart.scss'])
 
 @section('content')
 
-@vite(['resources/sass/cart.scss'])
+<div class="content">
+  <div class="content-wrapper">
+  <h1>Your cart</h1>
+  <div class="cart-contents">
+    <table>
+      <tr>
+        <th style="width:50%">Product</th>
+        <th style="width:10%">Price</th>
+        <th style="width:8%">Quantity</th>
+        <th style="width:22%" class="text-center">Subtotal</th>
+        <th style="width:10%"></th>
+      </tr>
 
-<main class="content">
-  <div class="content-wrapper cart px-6 mx-auto">
-      <div class="flex justify-center my-6">
-          <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
-            @if ($message = Session::get('success'))
-                <div class="p-4 mb-3 bg-green-400 rounded">
-                    <p class="text-green-800">{{ $message }}</p>
-                </div>
-            @endif
-              <h3 class="text-3xl text-bold">Cart List</h3>
-            <div class="flex-1">
-              <table class="w-full text-sm lg:text-base" cellspacing="0">
-                <thead>
-                  <tr class="h-12 uppercase">
-                    <th class="hidden md:table-cell"></th>
-                    <th class="text-left">Name</th>
-                    <th class="pl-5 text-left lg:text-right lg:pl-0">
-                      <span class="lg:hidden" title="Quantity">Qtd</span>
-                      <span class="hidden lg:inline">Quantity</span>
-                    </th>
-                    <th class="hidden text-right md:table-cell"> price</th>
-                    <th class="hidden text-right md:table-cell"> Remove </th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach ($cartItems as $item)
-                  <tr>
-                    <td class="hidden pb-4 md:table-cell">
-                      <a href="#">
-                        <img src="{{ $item->attributes->image }}" class="w-20 rounded" alt="Thumbnail">
-                      </a>
-                    </td>
-                    <td>
-                      <a href="#">
-                        <p class="mb-2 md:ml-4">{{ $item->name }}</p>
-                        
-                      </a>
-                    </td>
-                    <td class="justify-center mt-6 md:justify-end md:flex">
-                      <div class="h-10 w-28">
-                        <div class="relative flex flex-row w-full h-8">
-                          
-                          <form action="{{ route('cart.update') }}" method="POST">
-                            @csrf
-                            <input type="hidden" name="id" value="{{ $item->id}}" >
-                          <input type="number" name="quantity" value="{{ $item->quantity }}" 
-                          class="w-6 text-center bg-gray-300" />
-                          <button type="submit" class="px-2 pb-2 ml-2 text-white bg-blue-500">update</button>
-                          </form>
+      <?php $total = 0 ?>
+        @if(session('cart'))
+            @foreach(session('cart') as $id => $details)
+                <?php $total += $details['price'] * $details['quantity'] ?>
+                <tr>
+                    <td data-th="Product">
+                        <div class="row">
+                            <div class="col-sm-3 hidden-xs"><img src="{{ $details['photo'] }}" width="100" height="100" class="img-responsive"/></div>
+                            <div class="col-sm-9">
+                                <h4 class="nomargin">{{ $details['name'] }}</h4>
+                            </div>
                         </div>
-                      </div>
                     </td>
-                    <td class="hidden text-right md:table-cell">
-                      <span class="text-sm font-medium lg:text-base">
-                          ${{ $item->price }}
-                      </span>
+                    <td data-th="Price">${{ $details['price'] }}</td>
+                    <td data-th="Quantity">
+                        <input type="number" value="{{ $details['quantity'] }}" class="form-control quantity" />
                     </td>
-                    <td class="hidden text-right md:table-cell">
-                      <form action="{{ route('cart.remove') }}" method="POST">
-                        @csrf
-                        <input type="hidden" value="{{ $item->id }}" name="id">
-                        <button class="px-4 py-2 text-white bg-red-600">x</button>
-                    </form>
-                      
+                    <td data-th="Subtotal" class="text-center">${{ $details['price'] * $details['quantity'] }}</td>
+                    <td class="actions" data-th="">
+                        <button class="btn btn-info btn-sm update-cart" data-id="{{ $id }}"><i class="fa fa-refresh"></i></button>
+                        <button class="btn btn-danger btn-sm remove-from-cart" data-id="{{ $id }}"><i class="fa fa-trash-o"></i></button>
                     </td>
-                  </tr>
-                  @endforeach
-                    
-                </tbody>
-              </table>
-              <div>
-                Total: ${{ Cart::getTotal() }}
-              </div>
-              <div>
-                <form action="{{ route('cart.clear') }}" method="POST">
-                  @csrf
-                  <button class="px-6 py-2 text-red-800 bg-red-300">Remove All Cart</button>
-                </form>
-              </div>
-
-
-            </div>
-          </div>
-        </div>
+                </tr>
+            @endforeach
+        @endif
+        </tbody>
+    </table>
+    </table>
   </div>
-</main>
+</div>
+</div>
+
+
 
 @if(!auth()->user() || auth()->user()->role != "administrator")
     @include('layouts.footer')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
